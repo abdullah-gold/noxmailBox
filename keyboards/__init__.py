0df4afbb44e3604174b344c3ceaac46189a03ac3
abdullah-gold <hosne0009@gmail.com>
2026-06-29 11:52:48 +0300

    __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,17 @@
+from keyboards.keyboards import (
+    main_menu_kb, admin_main_menu_kb,
+    mail_type_kb, services_kb, confirm_purchase_kb,
+    accounts_pagination_kb,
+    admin_panel_kb, admin_back_kb, admin_services_kb,
+    admin_mail_type_kb, admin_upload_services_kb,
+    admin_manage_admins_kb, sold_pagination_kb,
+)
+
+__all__ = [
+    "main_menu_kb", "admin_main_menu_kb",
+    "mail_type_kb", "services_kb", "confirm_purchase_kb",
+    "accounts_pagination_kb",
+    "admin_panel_kb", "admin_back_kb", "admin_services_kb",
+    "admin_mail_type_kb", "admin_upload_services_kb",
+    "admin_manage_admins_kb", "sold_pagination_kb",
+]
