43550f818ee5f7e0b09e34a2f154d23bb51ef289
abdullah-gold <hosne0009@gmail.com>
2026-06-29 11:44:31 +0300

    __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+from handlers.common import router as common_router
+from handlers.buy import router as buy_router
+from handlers.my_accounts import router as my_accounts_router
+from handlers.admin import router as admin_router
+
+__all__ = ["common_router", "buy_router", "my_accounts_router", "admin_router"]
