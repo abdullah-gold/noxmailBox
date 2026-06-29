68e4398402373e0e1d86a042c6339bd4aa63591d
abdullah-gold <hosne0009@gmail.com>
2026-06-29 12:10:23 +0300

    __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+from utils.helpers import (
+    mail_label, format_account_delivery,
+    format_account_history, total_pages,
+)
+
+__all__ = [
+    "mail_label", "format_account_delivery",
+    "format_account_history", "total_pages",
+]
